ad45835f1f365a8f58ebb0363310a60274c4dd86
XiaoWang1212 <danny20041212@gmail.com>
2024-07-29 23:17:23 +0800

    project completed

--- code/main.py
+++ code/main.py
@@ -1,10 +1,13 @@
 from settings import *
 from sys import exit
+from os.path import join
 
 # components
 from game import Game
 from score import Score
 from preview import Preview
+
+from random import choice
 
 class Main:
     def __init__(self):
@@ -15,11 +18,29 @@ class Main:
         self.clock = pygame.time.Clock()
         pygame.display.set_caption('Tetric')
         
+        # shape
+        self.next_shapes = [choice(list(TETROMINOS.keys())) for shape in range(3)]
+        
         # components
-        self.game = Game()
+        self.game = Game(self.get_next_shape, self.update_score)
         self.score = Score()
         self.preview = Preview()
         
+        # audio
+        self.music = pygame.mixer.Sound(join('..','Tetris','sound','music.wav'))
+        self.music.set_volume(0.05)
+        self.music.play(-1)
+        
+    def update_score(self, lines, score, level):
+        self.score.lines = lines
+        self.score.score = score
+        self.score.level = level
+            
+    def get_next_shape(self):
+        next_shape = self.next_shapes.pop(0)
+        self.next_shapes.append(choice(list(TETROMINOS.keys())))
+        return next_shape
+    
     def run(self):
         while True:
             for event in pygame.event.get():
@@ -33,7 +54,7 @@ class Main:
             # components
             self.game.run()
             self.score.run()
-            self.preview.run()
+            self.preview.run(self.next_shapes)
             
             # updating the game
             pygame.display.update()
--- code/score.py
+++ code/score.py
@@ -1,5 +1,6 @@
 from settings import *
 import pygame
+from os.path import join
 
 class Score:
     def __init__(self):
@@ -7,5 +8,28 @@ class Score:
         self.surface = pygame.Surface((SIDEBAR_WIDTH, GAME_HEIGHT * SCORE_HEIGHT_FRACTION - PADDING))
         self.rect = self.surface.get_rect(bottomright = (WINDOW_WIDTH - PADDING, WINDOW_HEIGHT - PADDING))
         
+        # font
+        self.font = pygame.font.Font(join('..','Tetris','graphics','Russo_One.ttf'), 30)
+        
+        # increment
+        self.increment_height = self.surface.get_height() / 3
+    
+        # data
+        self.score = 0
+        self.lines = 0
+        self.level = 1
+        
+    def display_text(self, pos, text):
+        text_surface = self.font.render(f'{text[0]}: {text[1]}', True, 'white')
+        text_rect = text_surface.get_rect(center=pos)
+        self.surface.blit(text_surface, text_rect)
+        
     def run(self):
+        self.surface.fill(GRAY)
+        for idx, text in enumerate([('Score', self.score), ('Level', self.level), ('Lines', self.lines)]):
+            x = self.surface.get_width() / 2
+            y = self.increment_height / 2 + idx * self.increment_height
+            self.display_text((x, y), text)
+            
         self.display_surface.blit(self.surface, self.rect)
+        pygame.draw.rect(self.display_surface, LINE_COLOR, self.rect, 2, 2)
